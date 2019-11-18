7d7de22c3d455d008e44a951e4b49d7cc3dc927b
harmannkibue <harmannkibue@gmail.com>
2019-11-18 20:33:54 +0300

    Fixed all the server issues

--- server/route/index.js
+++ server/route/index.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
-router.get("/", (req, res) => {
-    res.send({ response: "Hey thereeeeee" }).status(200);
+router.get("*/", (req, res) => {
+    res.send({ response: "Server is up and running!" }).status(200);
 });
 
 module.exports = router;
+
